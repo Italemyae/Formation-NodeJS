1a16e8b25ffc71b0744b0f1faaff680622229e5b
Julien BERNAT <Julien.BERNAT87@gmail.com>
2023-09-13 09:06:51 +0100

    feat : modules

--- Modules/commonjs/main.js
+++ Modules/commonjs/main.js
@@ -1,1 +1,4 @@
+// const MyMath = require('./my-math.js'); // ancien
+import { sum } from './my-math.js'; // nouveau
+
 console.log(sum(1, 2)); // 3
--- Modules/commonjs/my-math.js
+++ Modules/commonjs/my-math.js
@@ -1,3 +1,12 @@
 function sum(a, b) {
   return a + b;
 }
+
+// globalThis.sum = sum;
+
+console.log('execute my-math.js');
+
+// exports.sum = sum; // ancien
+// console.log(exports);
+
+export { sum }; // nouveau
