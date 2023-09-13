9bd1707639bb99fb1f5350eac5dad7225efe9ff8
Julien BERNAT <Julien.BERNAT87@gmail.com>
2023-09-13 16:27:11 +0200

    feat : exo de ce matin fini

--- Exercice-Build/build.js
+++ Exercice-Build/build.js
@@ -1,8 +1,6 @@
-const fs = require('fs-extra');
 const path = require('path');
-const del = require('del');
 const md5 = require('md5');
-const UglifyJS = require('uglify-es');
+const { minify } = require('terser');
 
 const distPath = path.resolve(__dirname, 'dist');
 const srcPath = path.resolve(__dirname, 'src');
@@ -12,3 +10,8 @@ const indexHtmlPath = path.resolve(srcPath, 'index.html');
 const indexHtmlDistPath = path.resolve(distPath, 'index.html');
 const appJsDistPath = path.resolve(distPath, 'app.js');
 
+// import fs from 'node:fs/promises';
+// const fs = require('node:fs/promises')
+
+// 1 supprimer le dossier dist s'il exist
+// await fs.rm('dist', {'force':true, 'recursive':true}).catch((err) => console.error(err.message));
