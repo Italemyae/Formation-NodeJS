1907384f33de01ed6aa24d13d0781b73cf54feeb
Julien BERNAT <Julien.BERNAT87@gmail.com>
2023-09-13 16:52:15 +0200

    feat: terser minify

--- Exercice-Build/build.js
+++ Exercice-Build/build.js
@@ -19,15 +19,23 @@ async function build() {
   console.log(chalk.bgBlue('2 - créer dossier dist'));
   await fs.mkdir('dist', { recursive: true });
 
-  fs.readFile(horlogeJsPath)
+  await fs.readFile(horlogeJsPath)
     .then( (buffer) => fs.writeFile(appJsDistPath, buffer))
     .then( () => console.log(chalk.bgBlue("3 - Copy horloge to app.js done")))
     .catch( (err) => console.error(err.message));
 
-  fs.readFile(indexJsPath)
+  await fs.readFile(indexJsPath)
     .then( (buffer) => fs.appendFile(appJsDistPath, buffer))
     .then( () => console.log(chalk.bgBlue("4 - Append index to app.js done")))
     .catch( (err) => console.error(err.message));
+
+  // et sans mélanger les syntaxes..
+  const buffer = await fs.readFile(appJsDistPath);
+  const minifiedCode = await minify(buffer.toString('utf8'));
+  await fs.writeFile(appJsDistPath.replace('.js', '.min.js'), minifiedCode.code);
+  console.log(chalk.bgBlue("5 - App.js minified"));
+
+
 }
 
 build();
