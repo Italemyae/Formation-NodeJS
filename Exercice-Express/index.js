6b85d3569ec0e702e12caedab7cf8f3305200762
Julien BERNAT <Julien.BERNAT87@gmail.com>
2023-09-14 15:10:57 +0200

    feat: route get

--- Exercice-Express/index.js
+++ Exercice-Express/index.js
@@ -13,10 +13,32 @@ const todos = [
   },
 ];
 
+function nextId(todosArray) {
+  let maxId = 0;
+
+  for (const todo of todosArray) {
+    if (todo.id > maxId) {
+      maxId = todo.id;
+    }
+  }
+
+  return maxId + 1;
+}
+
+
 const app = express();
 
-// Ajouter vos routes ici :
-// ...
+app.get('/api/todos', (req, res) => {
+  // res.statusCode = 200; //  (par défaut dans express)
+  console.table(todos);
+  res.json(todos)
+});
+
+app.get('/api/todos/:todoId', (req, res) => {
+  // res.statusCode = 200; //  (par défaut dans express)
+  console.table(todos.filter(todo => todo.id === Number(req.params.todoId)));
+  res.json(todos.filter(todo => todo.id === Number(req.params.todoId)));
+});
 
 app.listen(4000, () => {
   console.log('Server started on http://localhost:4000');
