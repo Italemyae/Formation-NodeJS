fdd13e4b8ede50eea067e766ba174a60cc0a4a08
Julien BERNAT <Julien.BERNAT87@gmail.com>
2023-09-15 11:56:51 +0200

    feat: func

--- Express-REST/models/todo.cjs
+++ Express-REST/models/todo.cjs
@@ -11,66 +11,86 @@ const todos = [
   }
 ];
 
-function generateId() {
-  const maxId = todos.reduce((acc, c) => c.id > acc ? c.id : acc, 0);
-  return maxId + 1;
-}
+// function generateId() {
+//   const maxId = todos.reduce((acc, c) => c.id > acc ? c.id : acc, 0);
+//   return maxId + 1;
+// }
 
-function find() {
-  return Promise.resolve(todos);
-}
+// function find() {
+//   return Promise.resolve(todos);
+// }
 
-function findById(id) {
-  id = Number(id);
-  const todo = todos.find((c) => c.id === id);
+// function findById(id) {
+//   id = Number(id);
+//   const todo = todos.find((c) => c.id === id);
 
-  if (!todo) {
-    return Promise.resolve(null);
-  }
+//   if (!todo) {
+//     return Promise.resolve(null);
+//   }
 
-  return Promise.resolve(todo);
-}
+//   return Promise.resolve(todo);
+// }
 
-function create(todo) {
-  todo.id = generateId();
+// function create(todo) {
+//   todo.id = generateId();
 
-  todos.push(todo);
+//   todos.push(todo);
 
-  return Promise.resolve(todo);
-}
+//   return Promise.resolve(todo);
+// }
 
-function findByIdAndDelete(id) {
-  id = Number(id);
-  const todo = todos.find((c) => c.id === id);
+// function findByIdAndDelete(id) {
+//   id = Number(id);
+//   const todo = todos.find((c) => c.id === id);
 
-  if (!todo) {
-    return Promise.resolve(null);
-  }
+//   if (!todo) {
+//     return Promise.resolve(null);
+//   }
 
-  const index = todos.indexOf(todo);
-  todos.splice(index, 1);
+//   const index = todos.indexOf(todo);
+//   todos.splice(index, 1);
 
-  return Promise.resolve(todo);
-}
+//   return Promise.resolve(todo);
+// }
 
 
-function findByIdAndUpdate(id, newTodo) {
-  id = Number(id);
-  const todo = todos.find((c) => c.id === id);
-  newTodo.id = id;
+// function findByIdAndUpdate(id, newTodo) {
+//   id = Number(id);
+//   const todo = todos.find((c) => c.id === id);
+//   newTodo.id = id;
 
-  if (!todo) {
-    return Promise.resolve(null);
-  }
+//   if (!todo) {
+//     return Promise.resolve(null);
+//   }
 
-  const index = todos.indexOf(todo);
-  todos[index] = newTodo;
+//   const index = todos.indexOf(todo);
+//   todos[index] = newTodo;
 
-  return Promise.resolve(todo);
-}
+//   return Promise.resolve(todo);
+// }
 
-exports.find = find;
-exports.findById = findById;
-exports.create = create;
-exports.findByIdAndDelete = findByIdAndDelete;
-exports.findByIdAndUpdate = findByIdAndUpdate;
+// exports.find = find;
+// exports.findById = findById;
+// exports.create = create;
+// exports.findByIdAndDelete = findByIdAndDelete;
+// exports.findByIdAndUpdate = findByIdAndUpdate;
+
+
+
+
+
+
+const mongoose  = require('mongoose');
+
+// const Todo = mongoose.model('Todo', { name: String }, todos);
+// prends directement le nom au pluriel
+// const Todo = mongoose.model('Todo', { name: String });
+
+const schema = new mongoose.Schema({
+  title: { type: String, required: true },
+  completed: { type: Boolean, default: false},
+});
+
+const Todo = mongoose.model('Todo', schema, 'todos'); // c'est une classe
+
+module.exports = Todo;
